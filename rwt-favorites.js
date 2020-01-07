4b6d41bd653141713e1883180b5f46995ee8064a
Joe Honton <joe@joe-honton.com>
2020-01-06 16:02:09 -0800

    no-referrer

--- rwt-favorites.js
+++ rwt-favorites.js
@@ -75,7 +75,7 @@ export default class RwtFavorites extends HTMLElement {
 	//< returns a document-fragment suitable for appending to shadowRoot
 	//< returns null if server does not respond with 200 or 304
 	async fetchTemplate() {
-		var response = await fetch('/node_modules/rwt-favorites/rwt-favorites.blue');
+		var response = await fetch('/node_modules/rwt-favorites/rwt-favorites.blue', {cache: "no-cache", referrerPolicy: 'no-referrer'});		// send conditional request to server with ETag and If-None-Match
 		if (response.status != 200 && response.status != 304)
 			return null;
 		var templateText = await response.text();
@@ -90,7 +90,7 @@ export default class RwtFavorites extends HTMLElement {
 	//< returns an style element suitable for appending to shadowRoot
 	//< returns null if server does not respond with 200 or 304
 	async fetchCSS() {
-		var response = await fetch('/node_modules/rwt-favorites/rwt-favorites.css');
+		var response = await fetch('/node_modules/rwt-favorites/rwt-favorites.css', {cache: "no-cache", referrerPolicy: 'no-referrer'});
 		if (response.status != 200 && response.status != 304)
 			return null;
 		var css = await response.text();
@@ -161,7 +161,7 @@ export default class RwtFavorites extends HTMLElement {
 			return;
 		var sourceref = this.getAttribute('sourceref');
 
-		var response = await fetch(sourceref, {cache: "no-cache"});		// send conditional request to server with ETag and If-None-Match
+		var response = await fetch(sourceref, {cache: "no-cache", referrerPolicy: 'no-referrer'});
 		if (response.status != 200 && response.status != 304)
 			return;
 		var jsonData = await response.json();
